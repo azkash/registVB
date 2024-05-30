412a3f2ae007b6b756632e4e4605012a228a580d
DycandX <karismaviki@gmail.com>
2024-05-30 15:21:17 +0700

    menambahkan fitur queue

--- queue.Designer.vb
+++ queue.Designer.vb
@@ -22,17 +22,222 @@ Partial Class queue_form
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle13 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle14 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle15 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Guna2HtmlLabel1 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2DataGridView1 = New Guna.UI2.WinForms.Guna2DataGridView()
+        Me.Guna2HtmlLabel5 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2HtmlLabel6 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2ShadowPanel1 = New Guna.UI2.WinForms.Guna2ShadowPanel()
+        Me.Label_NamaPasien = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2HtmlLabel4 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Label_Poli = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Label_Antrian = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Button_Next = New Guna.UI2.WinForms.Guna2Button()
+        Me.Button_RefreshAntrian = New Guna.UI2.WinForms.Guna2Button()
+        CType(Me.Guna2DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Guna2ShadowPanel1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'Guna2HtmlLabel1
+        '
+        Me.Guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2HtmlLabel1.Location = New System.Drawing.Point(878, 132)
+        Me.Guna2HtmlLabel1.Name = "Guna2HtmlLabel1"
+        Me.Guna2HtmlLabel1.Size = New System.Drawing.Size(276, 48)
+        Me.Guna2HtmlLabel1.TabIndex = 0
+        Me.Guna2HtmlLabel1.Text = "Antrian Pasien"
+        '
+        'Guna2DataGridView1
+        '
+        DataGridViewCellStyle13.BackColor = System.Drawing.Color.White
+        Me.Guna2DataGridView1.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle13
+        DataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle14.BackColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
+        DataGridViewCellStyle14.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle14.ForeColor = System.Drawing.Color.White
+        DataGridViewCellStyle14.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle14.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle14.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.Guna2DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle14
+        Me.Guna2DataGridView1.ColumnHeadersHeight = 4
+        Me.Guna2DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing
+        DataGridViewCellStyle15.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle15.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle15.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle15.ForeColor = System.Drawing.Color.FromArgb(CType(CType(71, Byte), Integer), CType(CType(69, Byte), Integer), CType(CType(94, Byte), Integer))
+        DataGridViewCellStyle15.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(229, Byte), Integer), CType(CType(255, Byte), Integer))
+        DataGridViewCellStyle15.SelectionForeColor = System.Drawing.Color.FromArgb(CType(CType(71, Byte), Integer), CType(CType(69, Byte), Integer), CType(CType(94, Byte), Integer))
+        DataGridViewCellStyle15.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.Guna2DataGridView1.DefaultCellStyle = DataGridViewCellStyle15
+        Me.Guna2DataGridView1.GridColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(229, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2DataGridView1.Location = New System.Drawing.Point(727, 212)
+        Me.Guna2DataGridView1.Name = "Guna2DataGridView1"
+        Me.Guna2DataGridView1.RowHeadersVisible = False
+        Me.Guna2DataGridView1.RowHeadersWidth = 51
+        Me.Guna2DataGridView1.RowTemplate.Height = 24
+        Me.Guna2DataGridView1.Size = New System.Drawing.Size(496, 394)
+        Me.Guna2DataGridView1.TabIndex = 5
+        Me.Guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White
+        Me.Guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.Font = Nothing
+        Me.Guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty
+        Me.Guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty
+        Me.Guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty
+        Me.Guna2DataGridView1.ThemeStyle.BackColor = System.Drawing.Color.White
+        Me.Guna2DataGridView1.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(229, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2DataGridView1.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2DataGridView1.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
+        Me.Guna2DataGridView1.ThemeStyle.HeaderStyle.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2DataGridView1.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White
+        Me.Guna2DataGridView1.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing
+        Me.Guna2DataGridView1.ThemeStyle.HeaderStyle.Height = 4
+        Me.Guna2DataGridView1.ThemeStyle.ReadOnly = False
+        Me.Guna2DataGridView1.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White
+        Me.Guna2DataGridView1.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal
+        Me.Guna2DataGridView1.ThemeStyle.RowsStyle.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2DataGridView1.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(71, Byte), Integer), CType(CType(69, Byte), Integer), CType(CType(94, Byte), Integer))
+        Me.Guna2DataGridView1.ThemeStyle.RowsStyle.Height = 24
+        Me.Guna2DataGridView1.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(229, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2DataGridView1.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(CType(CType(71, Byte), Integer), CType(CType(69, Byte), Integer), CType(CType(94, Byte), Integer))
+        '
+        'Guna2HtmlLabel5
+        '
+        Me.Guna2HtmlLabel5.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel5.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2HtmlLabel5.Location = New System.Drawing.Point(348, 30)
+        Me.Guna2HtmlLabel5.Name = "Guna2HtmlLabel5"
+        Me.Guna2HtmlLabel5.Size = New System.Drawing.Size(495, 48)
+        Me.Guna2HtmlLabel5.TabIndex = 8
+        Me.Guna2HtmlLabel5.Text = "NEXT CENTRAL CLINICS"
+        '
+        'Guna2HtmlLabel6
+        '
+        Me.Guna2HtmlLabel6.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel6.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2HtmlLabel6.Location = New System.Drawing.Point(139, 132)
+        Me.Guna2HtmlLabel6.Name = "Guna2HtmlLabel6"
+        Me.Guna2HtmlLabel6.Size = New System.Drawing.Size(388, 44)
+        Me.Guna2HtmlLabel6.TabIndex = 9
+        Me.Guna2HtmlLabel6.Text = "SEDANG DIPANGGIL"
+        '
+        'Guna2ShadowPanel1
+        '
+        Me.Guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2ShadowPanel1.Controls.Add(Me.Label_NamaPasien)
+        Me.Guna2ShadowPanel1.Controls.Add(Me.Guna2HtmlLabel4)
+        Me.Guna2ShadowPanel1.Controls.Add(Me.Label_Poli)
+        Me.Guna2ShadowPanel1.Controls.Add(Me.Label_Antrian)
+        Me.Guna2ShadowPanel1.FillColor = System.Drawing.Color.White
+        Me.Guna2ShadowPanel1.Location = New System.Drawing.Point(78, 212)
+        Me.Guna2ShadowPanel1.Name = "Guna2ShadowPanel1"
+        Me.Guna2ShadowPanel1.ShadowColor = System.Drawing.Color.Black
+        Me.Guna2ShadowPanel1.Size = New System.Drawing.Size(586, 230)
+        Me.Guna2ShadowPanel1.TabIndex = 10
+        '
+        'Label_NamaPasien
+        '
+        Me.Label_NamaPasien.BackColor = System.Drawing.Color.Transparent
+        Me.Label_NamaPasien.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_NamaPasien.Location = New System.Drawing.Point(150, 151)
+        Me.Label_NamaPasien.Name = "Label_NamaPasien"
+        Me.Label_NamaPasien.Size = New System.Drawing.Size(203, 53)
+        Me.Label_NamaPasien.TabIndex = 14
+        Me.Label_NamaPasien.Text = "________"
+        '
+        'Guna2HtmlLabel4
+        '
+        Me.Guna2HtmlLabel4.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel4.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2HtmlLabel4.Location = New System.Drawing.Point(150, 54)
+        Me.Guna2HtmlLabel4.Name = "Guna2HtmlLabel4"
+        Me.Guna2HtmlLabel4.Size = New System.Drawing.Size(20, 71)
+        Me.Guna2HtmlLabel4.TabIndex = 13
+        Me.Guna2HtmlLabel4.Text = "|"
+        '
+        'Label_Poli
+        '
+        Me.Label_Poli.BackColor = System.Drawing.Color.Transparent
+        Me.Label_Poli.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_Poli.Location = New System.Drawing.Point(199, 54)
+        Me.Label_Poli.Name = "Label_Poli"
+        Me.Label_Poli.Size = New System.Drawing.Size(275, 71)
+        Me.Label_Poli.TabIndex = 12
+        Me.Label_Poli.Text = "________"
+        '
+        'Label_Antrian
+        '
+        Me.Label_Antrian.BackColor = System.Drawing.Color.Transparent
+        Me.Label_Antrian.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_Antrian.Location = New System.Drawing.Point(29, 54)
+        Me.Label_Antrian.Name = "Label_Antrian"
+        Me.Label_Antrian.Size = New System.Drawing.Size(71, 71)
+        Me.Label_Antrian.TabIndex = 11
+        Me.Label_Antrian.Text = "__"
+        '
+        'Button_Next
+        '
+        Me.Button_Next.BorderRadius = 10
+        Me.Button_Next.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.Button_Next.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.Button_Next.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Button_Next.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Button_Next.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Button_Next.ForeColor = System.Drawing.Color.White
+        Me.Button_Next.Location = New System.Drawing.Point(502, 459)
+        Me.Button_Next.Name = "Button_Next"
+        Me.Button_Next.Size = New System.Drawing.Size(162, 45)
+        Me.Button_Next.TabIndex = 11
+        Me.Button_Next.Text = "NEXT"
+        '
+        'Button_RefreshAntrian
+        '
+        Me.Button_RefreshAntrian.BorderRadius = 10
+        Me.Button_RefreshAntrian.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.Button_RefreshAntrian.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.Button_RefreshAntrian.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Button_RefreshAntrian.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Button_RefreshAntrian.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Button_RefreshAntrian.ForeColor = System.Drawing.Color.White
+        Me.Button_RefreshAntrian.Location = New System.Drawing.Point(78, 459)
+        Me.Button_RefreshAntrian.Name = "Button_RefreshAntrian"
+        Me.Button_RefreshAntrian.Size = New System.Drawing.Size(162, 45)
+        Me.Button_RefreshAntrian.TabIndex = 13
+        Me.Button_RefreshAntrian.Text = "REFRESH ANTRIAN"
         '
         'queue_form
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1920, 1080)
+        Me.ClientSize = New System.Drawing.Size(1652, 983)
+        Me.Controls.Add(Me.Button_RefreshAntrian)
+        Me.Controls.Add(Me.Button_Next)
+        Me.Controls.Add(Me.Guna2ShadowPanel1)
+        Me.Controls.Add(Me.Guna2HtmlLabel6)
+        Me.Controls.Add(Me.Guna2HtmlLabel5)
+        Me.Controls.Add(Me.Guna2DataGridView1)
+        Me.Controls.Add(Me.Guna2HtmlLabel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "queue_form"
         Me.Text = "queue"
+        CType(Me.Guna2DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Guna2ShadowPanel1.ResumeLayout(False)
+        Me.Guna2ShadowPanel1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents Guna2HtmlLabel1 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2DataGridView1 As Guna.UI2.WinForms.Guna2DataGridView
+    Friend WithEvents Guna2HtmlLabel5 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2HtmlLabel6 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2ShadowPanel1 As Guna.UI2.WinForms.Guna2ShadowPanel
+    Friend WithEvents Guna2HtmlLabel4 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Label_Poli As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Label_Antrian As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Button_Next As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Label_NamaPasien As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Button_RefreshAntrian As Guna.UI2.WinForms.Guna2Button
 End Class
--- queue.vb
+++ queue.vb
@@ -1,3 +1,71 @@
-﻿Public Class queue_form
+﻿Imports MySql.Data.MySqlClient
 
+Public Class queue_form
+    Private lastProcessedId As Integer = -1 ' Variable to keep track of the last processed ID
+
+    Private Sub queue_form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadQueue()
+        UpdateNextButtonState()
+    End Sub
+
+    Private Sub LoadQueue()
+        Try
+            Dim dbConnection As New DatabaseConnection()
+            Using conn As MySqlConnection = dbConnection.GetConnection()
+                conn.Open()
+                Dim query As String = "SELECT pasien.id, pasien.nama, poli.nama_poli " &
+                                      "FROM pasien " &
+                                      "JOIN poli ON pasien.poli_id = poli.id " &
+                                      "WHERE pasien.id > @lastProcessedId " &  ' Fetch records with id greater than the last processed id
+                                      "ORDER BY pasien.id ASC"
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@lastProcessedId", lastProcessedId)
+                    Using adapter As New MySqlDataAdapter(cmd)
+                        Dim table As New DataTable()
+                        adapter.Fill(table)
+                        Guna2DataGridView1.DataSource = table
+                    End Using
+                End Using
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Terjadi kesalahan saat memuat data pasien: " & ex.Message)
+        End Try
+        UpdateNextButtonState()
+    End Sub
+
+    Private Sub Button_Next_Click(sender As Object, e As EventArgs) Handles Button_Next.Click
+        CommitDataGridViewEdits() ' Commit any uncommitted edits before proceeding
+
+        If Guna2DataGridView1.Rows.Count > 0 Then
+            Dim currentRow = Guna2DataGridView1.Rows(0)
+            If currentRow IsNot Nothing Then
+                Label_Antrian.Text = currentRow.Cells("id").Value?.ToString()
+                Label_Poli.Text = currentRow.Cells("nama_poli").Value?.ToString()
+                Label_NamaPasien.Text = currentRow.Cells("nama").Value?.ToString()
+                lastProcessedId = CInt(currentRow.Cells("id").Value) ' Update the last processed ID
+                Guna2DataGridView1.Rows.RemoveAt(0)
+            End If
+        Else
+            MessageBox.Show("Tidak ada antrian.", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+        UpdateNextButtonState()
+    End Sub
+
+    Private Sub Button_RefreshAntrian_Click(sender As Object, e As EventArgs) Handles Button_RefreshAntrian.Click
+        LoadQueue()
+    End Sub
+
+    Private Sub UpdateNextButtonState()
+        Button_Next.Enabled = Guna2DataGridView1.Rows.Count > 0
+    End Sub
+
+    Private Sub CommitDataGridViewEdits()
+        ' Ensure that any edits are committed before removing rows
+        If Guna2DataGridView1.IsCurrentCellInEditMode Then
+            Guna2DataGridView1.EndEdit()
+        End If
+        If Guna2DataGridView1.IsCurrentRowDirty Then
+            Guna2DataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit)
+        End If
+    End Sub
 End Class
