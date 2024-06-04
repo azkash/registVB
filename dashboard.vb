cd7f736f2358ed81840b937200dababafd722783
DycandX <karismaviki@gmail.com>
2024-06-04 21:01:15 +0700

    clearing testing6

--- dashboard.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class dashboard
-    Private Sub dashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        TextBox_LogedAs.Text = "" & MenuLogin.LoggedInUser
-    End Sub
-End Class
